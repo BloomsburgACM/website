a4ecc3d6f9b7daab47657bbcc90372d8d65ca5e5
Brian Fekete <bjf73558@huskies.bloomu.edu>
2016-07-10 21:00:34 -0400

    Initial commit of the website

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var module = angular.module('acmWebsite', ['ngRoute']);
+/**
+ *  Routes
+ */
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    // Home
+        .when("/", {
+            templateUrl: "partials/home.html"
+            , controller: "PageCtrl"
+        })
+        // Pages
+        .when("/about", {
+            templateUrl: "partials/about.html"
+            , controller: "PageCtrl"
+        }).when("/events", {
+            templateUrl: "partials/events.html"
+            , controller: "PageCtrl"
+        }).when("/teams", {
+            templateUrl: "partials/teams.html"
+            , controller: "PageCtrl"
+        }).when("/contact", {
+            templateUrl: "partials/contact.html"
+            , controller: "PageCtrl"
+        })
+        // else 404
+        .otherwise("/404", {
+            templateUrl: "partials/404.html"
+            , controller: "PageCtrl"
+        });
+}]);
+module.controller('PageCtrl', function ( /* $scope, $location, $http */ ) {
+    console.log("Page Controller reporting for duty.");
+    // Activates the Carousel
+    $('.carousel').carousel({
+        interval: 5000
+    });
+    // Activates Tooltips for Social Links
+    $('.tooltip-social').tooltip({
+        selector: "a[data-toggle=tooltip]"
+    })
+});
